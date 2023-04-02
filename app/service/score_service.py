3eb806673e56eca4d968f59813adf818c2c44766
Yannick Müller <yannick.mueller@fusiva.ch>
2023-04-02 13:24:19 +0200

    add endpoint to get all games of user

--- app/service/score_service.py
+++ app/service/score_service.py
@@ -3,13 +3,14 @@ import random
 from sqlalchemy.orm import Session
 from sqlalchemy import func
 from app.db import model
+from app.schemas import Score as HttpScore
 
 
 def get_highscore(db: Session) -> int:
     return db.query(func.max(model.Score.points)).first()[0]
 
 
-def insert_new_score(score: model.Score, db: Session) -> str:
+def insert_new_score(score: HttpScore, db: Session) -> str:
     if score.user_id is None:
         score.user_id = "%032x" % random.getrandbits(256)
     db_score = model.Score(score)
@@ -17,3 +18,8 @@ def insert_new_score(score: model.Score, db: Session) -> str:
     db.commit()
     db.refresh(db_score)
     return score.user_id
+
+
+def get_scores_of_user(user_id: str, db: Session) -> list[model.Score]:
+    return db.query(model.Score).filter(model.Score.user_id == user_id).all()
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
+from app.db import model
 from app.db.model import Base
 from app.schemas import Score
 from app.db.database import SessionLocal, engine
@@ -19,14 +20,31 @@ def get_db():
         db.close()
 
 
-@app.get("/highscore")
+@app.get("/highscore", )
 async def get_highscore(db: Session = Depends(get_db)) -> int:
+    """
+    Http Endpoint to get the highscore over all played games
+    """
     return score_service.get_highscore(db)
 
 
 @app.post("/")
 async def add_new_score(score: Score, db: Session = Depends(get_db)) -> str:
+    """
+    Http Endpoint to add a new Score to the collection. If no user_id is provied,
+    will the script automatic generate a random Userid
+    """
     if score.points < 0:
         raise HTTPException(detail="The score needs to be a positive value (>= 0)", status_code=409)
 
     return score_service.insert_new_score(score, db)
+
+
+@app.get("/")
+async def get_all_scores(user_id: str = "", db: Session = Depends(get_db)) -> list[Score]:
+    """
+    Http Endpoint to get all played games of a sigel User
+    """
+    if user_id == "":
+        raise HTTPException(detail="You have to provide a UserId", status_code=409)
+    return score_service.get_scores_of_user(user_id, db)
