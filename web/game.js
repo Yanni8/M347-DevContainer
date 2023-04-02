88b72599ac8058df6a4cbebdd8f449583f84ecdc
Yannick Müller <yannick.mueller@fusiva.ch>
2023-04-02 17:41:11 +0200

    add fist (basic) version of working game

--- web/game.js
+++ web/game.js
@@ -1,102 +1,100 @@
-var correctur_x = 1, correctur_y = 1;
-var snake = [];
-var snakeHeader;
-var lastKey;
-var snakeMove = [0,0];
+var correctur_x = 1, correctur_y = 1, snake = [], snakeHeader,apple,lastKey,snakeMove = [0, 0],intervalId;
 
-function renderRandomApple(canvas){
+function renderRandomApple(canvas) {
     let x = Math.random() * 4800;
     let y = Math.random() * 4800;
-    x = x - (x % 200);
-    y = y - (y % 200);
-
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "#800303";
-    ctx.fillRect(x*correctur_x,y*correctur_y,200 * correctur_x,200 * correctur_y);
-
-    return ctx;
+    return drawShape(x - (x % 200), y - (y % 200), canvas, "#800303");
 }
 
 
-function resize(){
+function resize() {
     let tmp_y = window.innerWidth * 0.9;
     let tmp_x = (window.innerHeight - 160) * 0.8;
 
-    if(tmp_x > tmp_y){
+    if (tmp_x > tmp_y) {
         correctur_y = tmp_y / tmp_x;
-    } else{
+    } else {
         correctur_x = tmp_x / tmp_y;
     }
 }
 
-function drawShape(x,y, canvas, color){
+function drawShape(x, y, canvas, color) {
     const ctx = canvas.getContext("2d");
     ctx.fillStyle = color;
-    ctx.fillRect(x*correctur_x,y*correctur_y,200 * correctur_x,200 * correctur_y);
-    return [x,y];
+    ctx.fillRect(x * correctur_x, y * correctur_y, 200 * correctur_x, 200 * correctur_y);
+    return [x, y];
 }
 
-function moveSnake(newX, newY, canvas){
-    
+function moveSnake(newX, newY, canvas) {
+
     let elementToRemove = snake.at(0);
-    snake.shift(); 
+    snake.shift();
     drawShape(elementToRemove[0], elementToRemove[1], canvas, "#636363");
     snake.push(drawShape(snakeHeader[0], snakeHeader[1], canvas, "#00a329"))
     snakeHeader = drawShape(newX, newY, canvas, "#ffffff");
 }
 
-function initSnake(canvas){
-    snake.push(drawShape(0,0,canvas, "#00a329"))
+function initSnake(canvas) {
+    snake.push(drawShape(0, 0, canvas, "#00a329"))
 
-    snake.push(drawShape(200,0,canvas, "#00a329"))
+    snake.push(drawShape(200, 0, canvas, "#00a329"))
 
-    snakeHeader = drawShape(400,0,canvas, "#ffffff")
+    snakeHeader = drawShape(400, 0, canvas, "#ffffff")
 
-    snakeMove = [200,0];
+    snakeMove = [200, 0];
+    apple = renderRandomApple(canvas);
 }
 
-function gameLoop(canvas){
-    let currentX = 400, currentY = 0;
 
+function afterChecks(canvas) {
+    if (snakeHeader[0] === apple[0] && snakeHeader[1] == apple[1]) {
+        console.log("Ate apple");
+        snake.unshift([-300,-300]);
+        console.log(snake);
+        apple = renderRandomApple(canvas);
+    }
+    else if (snakeHeader[0] * correctur_x >= 5000 || snakeHeader[0] < 0 || snakeHeader[1] * correctur_y >= 5000 || snakeHeader[1] < 0) {
+        window.clearInterval(intervalId);
+    }
 }
 
 function run() {
     console.log("Starting now Game");
     var canvas = null;
-    while(canvas == null){
+    while (canvas == null) {
         canvas = document.getElementById("game");
         console.log("Wating until canvas is loadet")
     }
     resize();
-    console.log(correctur_x,correctur_y);
+    console.log(correctur_x, correctur_y);
     initSnake(canvas);
-    gameLoop(canvas);
     //renderRandomApple(canvas);
-    window.setInterval(function(){
-        if(lastKey === "ArrowRight" && snakeMove[0] !== -200){
-            snakeMove = [200,0];
+    intervalId = window.setInterval(function () {
+        if (lastKey === "ArrowRight" && snakeMove[0] !== -200) {
+            snakeMove = [200, 0];
         }
 
-        if(lastKey === "ArrowLeft" && snakeMove[0] !== 200){
-            snakeMove = [-200,0];
+        if (lastKey === "ArrowLeft" && snakeMove[0] !== 200) {
+            snakeMove = [-200, 0];
         }
 
-        if(lastKey === "ArrowUp" && snakeMove[1] !== 200){
-            snakeMove = [0,-200];
+        if (lastKey === "ArrowUp" && snakeMove[1] !== 200) {
+            snakeMove = [0, -200];
         }
 
-        if(lastKey === "ArrowDown" && snakeMove[1] !== -200){
-            snakeMove = [0,200];
+        if (lastKey === "ArrowDown" && snakeMove[1] !== -200) {
+            snakeMove = [0, 200];
         }
         moveSnake(snakeHeader[0] + snakeMove[0], snakeHeader[1] + snakeMove[1], canvas);
         lastKey = undefined;
+        afterChecks(canvas);
     }, 100)
 }
 
 run();
 
-window.addEventListener('keydown', function(event) {
-    if(event.key.startsWith("Arrow")){
+window.addEventListener('keydown', function (event) {
+    if (event.key.startsWith("Arrow")) {
         lastKey = event.key;
     }
 });
