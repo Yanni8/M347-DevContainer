19241fd174bc777b1b23127d4c276cb2d2c62f80
Yannick Müller <yannick.mueller@fusiva.ch>
2023-04-04 17:08:28 +0200

    made watcher singelton

--- DevEnvironnement/dependencyWatcher.cpp
+++ DevEnvironnement/dependencyWatcher.cpp
@@ -3,72 +3,85 @@
 #include <memory>
 #include <string>
 #include <openssl/sha.h>
+#include <sys/file.h>
+#include <errno.h>
 
 #define LOG(x) std::cout << (x) << std::endl
 
-std::string exec(const char* cmd) {
+std::string exec(const char *cmd)
+{
     std::array<char, 128> buffer;
     std::string result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
 
-
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+    {
         result += buffer.data();
     }
     return result;
 }
 
 /**
- * Method to get the md5 hash value of all libraries
-*/
-std::string getHash(){
-    std::string libraries = exec("python3 -m pip freeze");
+ * Method to get sha256 hash
+ */
+std::string getSha256Hash(std::string &content)
+{
 
     unsigned char hash[SHA256_DIGEST_LENGTH];
-   
-    SHA256((const unsigned char*)(libraries.c_str()), libraries.length(), hash);
 
+    SHA256((const unsigned char *)(content.c_str()), content.length(), hash);
 
-	char hexString[SHA256_DIGEST_LENGTH];
-	 
-	for(int i = 0; i < (SHA256_DIGEST_LENGTH/2) - 1; i++){
-        sprintf(&hexString[i*2], "%02x", (unsigned int)hash[i]);
+    char hexString[SHA256_DIGEST_LENGTH];
+
+    for (int i = 0; i < (SHA256_DIGEST_LENGTH / 2) - 1; i++)
+    {
+        sprintf(&hexString[i * 2], "%02x", (unsigned int)hash[i]);
     }
-    
+
     return hexString;
 }
 
-
-void updateRequirements(){
+void updateRequirements()
+{
     system("python3 -m pip freeze > requirements.txt");
 }
 /**
  * Method to constantly check if a new library got added
-*/
-inline void checkDependencyLoop(){
-    std::string lastHash = getHash();
-
+ */
+inline void checkDependencyLoop()
+{
     while (1)
     {
-        std::string currentHash = getHash();
+        std::string pipLibraries = exec("python3 -m pip freeze");
+        std::string pipHash = getSha256Hash(pipLibraries);
+        std::string fileLibraries = exec("cat requirements.txt");
+        std::string fileHash = getSha256Hash(fileLibraries);
 
-        if(lastHash != currentHash){
-            //LOG("Dependencys got Updated");
+        if (fileHash != pipHash)
+        {
+            // LOG("Dependencys got Updated");
             updateRequirements();
         }
-
-        lastHash = currentHash;
     }
-    
-    
 }
-
 
 /**
  * Programm to automatic update the
- * requirmenets.txt file insed the 
+ * requirmenets.txt file insed the
  * dev container
-*/
-int main(int argc, char *argv[]){
-    checkDependencyLoop();
+ */
+int main(int argc, char *argv[])
+{
+
+    int pid_file = open("/var/run/dependencywatcher.pid", O_CREAT | O_RDWR, 0666);
+    int rc = flock(pid_file, LOCK_EX | LOCK_NB);
+    if (rc)
+    {
+        if (EWOULDBLOCK == errno)
+            LOG("Watcher already running...");
+    }
+    else
+    {
+        checkDependencyLoop();
+    }
 }
